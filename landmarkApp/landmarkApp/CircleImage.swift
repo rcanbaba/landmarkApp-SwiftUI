6c2fb4cf26a42d37cabb7242e8a14991964438e3
Can Babaoğlu <canbabaoglu@scorpapp.com>
2022-05-06 19:36:56 +0300

    basic SwiftUI implementations completed.

---
+++ landmarkApp/landmarkApp/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  landmarkApp
+//
+//  Created by Can Babaoğlu on 6.05.2022.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4.0)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
---
+++ landmarkApp/landmarkApp/landmarkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarkAppApp.swift
+//  landmarkApp
+//
+//  Created by Can Babaoğlu on 6.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct landmarkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
